b2066c47367d1b0765efb73f3a5ba3f9ccbd110b
Halfdan Hem <halfdanhem@gmail.com>
2019-10-04 16:07:57 -0700

    3.0.0 - Turn the no-react-select and  button-has-text-or-child rules on

--- rules/7geese/on.js
+++ rules/7geese/on.js
@@ -5,5 +5,7 @@ module.exports = {
         '7g/no-classname-on-common-components': 'error',
         '7g/no-sg-in-components': 'warn',
         '7g/redux-connect-filenames': 'error',
+        '7g/button-has-text-or-child': 'warn',
+        '7g/no-react-select': 'error',
     },
 };
