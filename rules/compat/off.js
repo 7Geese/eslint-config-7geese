87a8cdf8ac433f12ed80bb1953d2cc765329275b
layton <layton@7geese.com>
2021-02-09 14:07:52 -0800

    update to latest pacakges. simplify on/off pattern to just all rules

--- rules/compat/off.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    rules: {
-        'compat/compat': 'off',
-    },
-};
---
+++ rules/eslint.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+        './eslint/best-practices.js',
+        './eslint/errors.js',
+        './eslint/es6.js',
+        './eslint/node.js',
+        './eslint/strict.js',
+        './eslint/style.js',
+        './eslint/variables.js',
+    ],
+};
